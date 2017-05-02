fd9bde0d0621e2c789e6d48c4be8b2794d4ced8e
charli elwonger <celwonger@gmail.com>
2017-05-01 19:54:37 -0500

    button for new patch visible

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,11 +2,19 @@
 import React, { Component } from 'react';
 
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-      <header className="app-header">
-        <h1 className="app-title">bramble</h1>
-      </header>
+      <div className="app-top-area">
+        <header className="app-header">
+          <h1 className="app-title">bramble</h1>
+        </header>
+        <div className="header-controls">
+          <button type="button">+ new patch</button>
+        </div>
+      </div>
     );
   }
 }
