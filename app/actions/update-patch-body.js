d737b9c7542866a40ed81760a8bc04e9152b83b8
charli elwonger <celwonger@gmail.com>
2017-05-03 00:23:32 -0500

    worked on enter text screen

---
+++ app/actions/update-patch-body.js
@@ -0,0 +1,15 @@
+import utils from '../utils.js';
+
+export default function updatePatchBody(e) {
+  return (dispatch, getState) => {
+    // const newPatch = new Patch({
+    //   patchId: getState().bramble.patchCounter + 1,
+    //   name: 'New Patch',
+    //   body: 'example text'
+    // });
+    // dispatch({
+    //   type: 'ADD_PATCH',
+    //   newPatch: newPatch
+    // });
+  };
+}
--- app/components/PatchEdit.js
+++ app/components/PatchEdit.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import stateReturn from '../store/state-return.js';
 import utils from '../utils.js';
+import updatePatchBody from '../actions/update-patch-body.js';
 // import { Route, Link, NavLink } from 'react-router-dom';
 // import container from '../../containers/all.js';
 
@@ -11,11 +12,24 @@ class PatchEdit extends React.Component {
   constructor(props) {
     super(props);
     this.onClickToClose = this.onClickToClose.bind(this);
+    this.enterText = this.enterText.bind(this);
+    this.auto_grow = this.auto_grow.bind(this);
   }
 
   onClickToClose() {
     console.log('closing patch edit!');
     this.props.history.push('/');
+  }
+
+  enterText(e) {
+    this.auto_grow(e.target);
+    // this.props.dispatch({ type: 'UPDATE_CURRENT_PATCH', rawText: e.target.value });
+    this.props.dispatch(updatePatchBody(e));
+  }
+
+  auto_grow(element) {
+    element.style.height = '5px';
+    element.style.height = element.scrollHeight + 'px';
   }
 
   render() {
@@ -33,6 +47,7 @@ class PatchEdit extends React.Component {
             <div className="editor-container">
               <section className="patch-entry">
                 <textarea
+                  onChange={this.enterText}
                   className="patch-input"
                   id="patch-raw-text"
                   name="patch raw text in markdown"
@@ -45,13 +60,11 @@ class PatchEdit extends React.Component {
                 <button type="submit">Submit post</button>
               </section>
               {/*<section className="markdown-preview-section">
-              <article
-                className="markdown-article"
-                dangerouslySetInnerHTML={this.createMarkup(
-                  this.props.markdownApp.draft
-                )}
-              />
-            </section>*/}
+                <article
+                  className="markdown-article"
+                  dangerouslySetInnerHTML={this.createMarkup(currentPatch.body)}
+                />
+              </section>*/}
             </div>
           </div>
         </div>
