c0cd73f2bdee57b39bed875e7fe59a9ca9e25660
charli elwonger <celwonger@gmail.com>
2017-05-05 15:22:25 -0500

    clicking to delete a patch from patch prview no longer brings up modal overlay

--- app/models/patch.js
+++ app/models/patch.js
@@ -1,5 +1,5 @@
 export default function Patch(attributes) {
   this.patchId = attributes.patchId;
-  this.name = attributes.name || 'New Patch';
+  this.name = attributes.name;
   this.body = attributes.body;
 }
