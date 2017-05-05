ae38b60ce63e8f8f99f158f9adeff9fdc947ee50
charli elwonger <celwonger@gmail.com>
2017-05-05 18:07:38 -0500

    put in grid background

--- app/models/patch.js
+++ app/models/patch.js
@@ -3,3 +3,10 @@ export default function Patch(attributes) {
   this.name = attributes.name;
   this.body = attributes.body;
 }
+
+/* TODO:
+- add property to keep track of position, look at trello cards for reference
+- add method to set the position more easily!
+  (maybe this can be done without reducer?
+  and then it would dispatch a reducer action after raising the mouse button?)
+*/
