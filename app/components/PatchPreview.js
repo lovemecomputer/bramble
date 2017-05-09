e3284bd59b9d29845764c872ac9a66309dcaf578
charli elwonger <celwonger@gmail.com>
2017-05-08 19:47:17 -0500

    beginnings of click and drage

--- app/components/PatchPreview.js
+++ app/components/PatchPreview.js
@@ -2,22 +2,101 @@
 import React from 'react';
 // import { Route } from 'react-router-dom';
 
+// click and drag derived from https://stackoverflow.com/questions/20926551/recommended-way-of-making-react-component-div-draggable
+
 class PatchPreview extends React.Component {
   constructor(props) {
     super(props);
     this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    this.handleClickForDrag = this.handleClickForDrag.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
     this.renderDeleteButton = this.renderDeleteButton.bind(this);
+    this.state = {
+      dragging: false,
+      position: {
+        x: this.props.xPos,
+        y: this.props.yPos
+      },
+      relative: {
+        x: 0,
+        y: 0
+      } // position relative to the cursor
+    };
+  }
+
+  componentDidUpdate() {
+    if (this.state.dragging) {
+      document.addEventListener('mousemove', this.onMouseMove);
+      document.addEventListener('mouseup', this.onMouseUp);
+    } else if (!this.state.dragging) {
+      document.removeEventListener('mousemove', this.onMouseMove);
+      document.removeEventListener('mouseup', this.onMouseUp);
+    }
+  }
+
+  handleClickForDrag(event) {
+    // console.log('>>>> CLICK FOR DRAG>>', this);
+    // console.log('>>> ref >>>', this.refs.patchPreview);
+    if (event.button !== 0) return;
+    var position = {
+      x: this.refs.patchPreview.offsetLeft,
+      y: this.refs.patchPreview.offsetTop
+    };
+    console.log(
+      '\n\n\n\n\n||||||||||| START CLICK |||||||||||||\n',
+      '\nposition from offset\n',
+      {
+        x: this.refs.patchPreview.offsetLeft,
+        y: this.refs.patchPreview.offsetTop
+      },
+      '\nSTATE POSITION \n',
+      this.state.position,
+      '\nSTATE RELATIVE\n',
+      this.state.relative
+    );
+    this.setState({
+      dragging: true,
+      relative: {
+        x: event.pageX - this.state.position.x,
+        y: event.pageY - this.state.position.y
+      }
+    });
+    console.log('>>> check in after setting::: \n', this.state.position.y);
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  onMouseMove(event) {
+    console.log(
+      '\n\n\n>>> EVENT POSITION >>\n',
+      { x: event.pageX, y: event.pageY },
+      '\nSTATE POSITION \n',
+      this.state.position,
+      '\nSTATE RELATIVE\n',
+      this.state.relative
+    );
+    if (!this.state.dragging) return;
+    this.setState({
+      position: {
+        x: event.pageX - this.state.relative.x,
+        y: event.pageY - this.state.relative.y
+      }
+    });
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  onMouseUp(event) {
+    this.setState({ dragging: false });
+    event.stopPropagation();
+    event.preventDefault();
   }
 
   handleKeyPress(target) {
     if (target.charCode == 13) {
       this.props.openPatchEdit();
     }
-  }
-
-  handleDeleteClick(deleteFunction, eventToStop) {
-    deleteFunction();
   }
 
   renderDeleteButton() {
@@ -38,9 +117,16 @@ class PatchPreview extends React.Component {
       <div
         className="patch-preview"
         id={`patch-preview-${this.props.patchId}`}
+        ref={`patchPreview`}
         onClick={this.props.openPatchEdit}
         onKeyPress={this.handleKeyPress}
-        style={{ top: this.props.yPos, left: this.props.xPos}}
+        onMouseDown={event => {
+          this.handleClickForDrag(event);
+        }}
+        style={{
+          top: this.state.position.y,
+          left: this.state.position.x
+        }}
         tabIndex="2"
       >
         <header>
