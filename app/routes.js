c1ec0f3045e330ff5becc1ffed780f042dda8961
charli elwonger <celwonger@gmail.com>
2017-06-21 17:40:18 -0500

    clean up some code and updated some properties

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,11 @@
 /* eslint flowtype-errors/show-errors: 0 */
+
+// import react stuff
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
+
+// import components
 import App from './containers/App';
 import Header from './components/Header.js';
 import Patchboard from './components/Patchboard.js';
@@ -9,7 +13,6 @@ import PatchEdit from './components/PatchEdit.js';
 import ClosingModal from './components/ClosingModal.js';
 import Preview from './components/Preview.js';
 
-import HomePage from './containers/HomePage';
 
 function wrapperClassNames() {
   let classNames = ['app-wrapper'];
@@ -17,19 +20,6 @@ function wrapperClassNames() {
     classNames.push('modal-visible');
   return classNames.join(' ');
 }
-
-// <Router>
-//   <App>
-//     <div className={wrapperClassNames()}>
-//       <Header />
-//       <Route path="/" component={Patchboard} />
-//       <Route path="/patch-edit/:patchId" component={PatchEdit} />
-//       <Route path="/patch-edit/closing-edit" component={ClosingModal} />
-//       <Route exact path="/preview" component={Preview} />
-//       <Route exact path="/preview/:id" component={Preview} />
-//     </div>
-//   </App>
-// </Router>
 
 export default () => (
   <Router>
